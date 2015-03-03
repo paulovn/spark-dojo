27d57bc7068db066f54f6969ff4fcecb89b8063f
Gregorio Escalada <goyo@samson01.hi.inet>
2015-03-03 09:58:30 +0100

    scala version for wordcount Kata

---
+++ scala/CountingWords.2/build.sbt
@@ -0,0 +1,9 @@
+name := "ML-CountingWords.2"
+
+organization := "es.tid"
+
+version := "1.0"
+
+scalaVersion := "2.10.1"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.1.1"
---
+++ scala/CountingWords.3/src/main/scala/es/tid/ML/CountWords3.scala
@@ -0,0 +1,42 @@
+package es.tid.ML
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+
+object CountWords {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("CountWords")
+    val sc = new SparkContext(conf)
+
+    // text file in HDFS
+    val textFile = "hdfs:///user/samson/test/quijote.utf8.txt"
+
+    // Read the file from HDFS into a RDD                                                            
+    val lines = sc.textFile(textFile)
+
+    // Create a new RDD with all the words in the file (by splitting the lines in our RDD)
+    val words = lines.flatMap(line => line.trim.split(" "))
+
+    // Removing punctuation marks
+    // We will use a regular expression
+    val puntuationMarks = "[¡!\"\'#%,-.:;?¿{}]"
+    val filteredWords = words.map(word => word.replaceAll(puntuationMarks, ""))
+
+    // Turn the words RDD into a (key,value) RDD, in which the key is the word and value is 1
+    val wordsKeyValue = filteredWords.map(word => (word, 1))
+
+    // Reduce by adding the values for all records with the same key
+    val counts = wordsKeyValue.reduceByKey( _ + _ ).collect
+
+    //  And sort by frequency                                                                         
+    val orderedCounts = counts.sortWith(_._2 > _._2)
+
+    // Format output string 
+    val formattedOutput = orderedCounts.map{ case (word, count) => f"[${word}] : ${count}" }
+
+    // Save output to local file
+    val outputFile = "quijote-words-3.txt"
+    sc.parallelize(formattedOutput).saveAsTextFile(outputFile)
+  }
+}
+
